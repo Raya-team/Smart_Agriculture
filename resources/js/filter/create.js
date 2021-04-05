a68cd73b7dbce15e726c8ef5ddee271b82790243
Ehsan <ehsanroozbakhsh@gmail.com>
2021-04-05 13:48:21 +0430

    Ehsan

--- resources/js/filter/create.js
+++ resources/js/filter/create.js
@@ -8,9 +8,9 @@
 $(document).ready(function(){
     var colors = [];
     $('#btnAdd').click(function(){
-        var x = $('#color-select').val();
+        // var x = $('#color-select').val();
         var y = $('#color-select').find(':selected').data('color');
-        var fnCell = $('<td class="del1"><div  style="background-color:' +x+ ';width: 20px;height: 20px; border-radius: 100%; margin-top: 2px; margin-right: auto; margin-left: auto;"></div></td>');
+        var fnCell = $('<td class="del1"><div  style="background-color:' +y+ ';width: 20px;height: 20px; border-radius: 100%; margin-top: 2px; margin-right: auto; margin-left: auto;"></div></td>');
         var row = $('<tr></tr>');
 
         if (colors.find(item => item === y)){
