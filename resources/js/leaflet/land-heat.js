90478dbf0b9ecb9b8fbfbf92c259463c769b6a59
Ehsan <ehsanroozbakhsh@gmail.com>
2021-04-14 15:30:27 +0430

    Ehsan

--- resources/js/leaflet/land-heat.js
+++ resources/js/leaflet/land-heat.js
@@ -37,7 +37,21 @@ var details = JSON.parse( detailjson);
 $("#filter_id").change(function () {
 
     var filters = $(this).find(':selected').val();
-
+    var max1 = $(this).find(':selected').data('max');
+    var min1 = $(this).find(':selected').data('min');
+    var max2 = 1;
+    var min2 = 0;
+    if( max1 < 0)
+    {
+        max1 = Math.abs(min1) - Math.abs(max1);
+        min1 = 0;
+        console.log(max1);
+    }
+    else if( min1 < 0 ){
+        max1 = Math.abs(min1) + Math.abs(max1);
+        min1 = 0;
+        console.log(max1);
+    }
     var arr = [];
 
     for(var d=0;d<details.length;d++)
@@ -45,10 +59,12 @@ $("#filter_id").change(function () {
         if(details[d]['filter_id'] == filters)
         {
             var loc=(details[d]['location']);
-            var val=(details[d]['value']);
+            var val1=(details[d]['value']);
+            /*Value2 = (Value1 – Min1) * (Max2 – Min2) / (Max1 – Min1) + Min2*/
+            var val2 = Math.abs((val1 - Math.abs(min1)) * (max2 - min2) / (max1 - min1) + min2);
             //convert string to array
-            arr.push(JSON.parse( loc));
-            arr.push(JSON.parse( val));
+            arr.push(JSON.parse(loc));
+            arr.push(JSON.parse(val2));
         }
 
     }
--- resources/views/admin/lands/heat.blade.php
+++ resources/views/admin/lands/heat.blade.php
@@ -17,7 +17,7 @@
                         <select class="form-control chosen-select" style="width: 100%;" name="filter_id" id="filter_id" data-placeholder="فیلتر خود را انتخاب کنید">
                             <option value=""></option>
                             @foreach($filters as $filter)
-                                <option value="{{ $filter->id }}" data-filters="{{ $filter->nickname }}" >{{$filter->nickname}}</option>
+                                <option value="{{ $filter->id }}" data-max="{{ $filter->max }}" data-min="{{ $filter->min }}" >{{$filter->nickname}}</option>
                             @endforeach
                         </select>
                     </div>
